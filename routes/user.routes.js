bb65f68954b603692d17b954750d0d91c36115d4
miguelreched <121823965+miguelreched@users.noreply.github.com>
2023-11-10 11:44:16 +0100

    menudolio

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -18,20 +18,38 @@ router.post("/signup", async(req, res, next)=>{
         res.status(400).render("signup.hbs", {
             errMess: "All fields must be filled out"
         })
+        return;
     }
-    return;
+
+    const passwordSeg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+
+    if(passwordSeg.test(password) === false){
+        res.status(400).render("signup.hbs", {
+            errMess: "Password is not secure enough. It should have al least 8 characters, one uppercase, one lowercase and one number"
+        })
+        return;
+    }
+     
+    try {
+        const repeatedMail = await User.findOne({email})
+        if (repeatedMail !== null){
+            res.status(400).render("signup.hbs", {
+                errMess: "This email has already been used"
+            })
+            return
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const cryptPassword = await bcrypt.hash(password, salt)
+
+        await User.create({username, email, password, country})
+
+        res.redirect("profile.hbs")
+
+    }
+    catch(err){
+        next(err)
+    }    
 })
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
