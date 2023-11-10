df9f214a3325e4178b7f3a7d18f4c1dc44f0a42a
miguelreched <121823965+miguelreched@users.noreply.github.com>
2023-11-10 17:15:34 +0100

    arreglao

--- config/index.js
+++ config/index.js
@@ -20,7 +20,8 @@ const path = require("path");
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/name-of-your-app";
+const MONGO_URI = 
+process.env.MONGO_URI || "mongodb://127.0.0.1:27017/name-of-your-app";
 
 // Middleware configuration
 module.exports = (app) => {
