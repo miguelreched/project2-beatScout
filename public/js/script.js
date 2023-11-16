f8108060bb3d65232163d7b8074c018cc777b45c
justaregularchalo <gonzalo.lara.caceres@gmail.com>
2023-11-16 23:35:24 +0100

    estilos medio ready

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,28 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("name-of-your-app JS imported successfully!");
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("name-of-your-app JS imported successfully!");
+// });
+
+// document.querySelector("#show-form").addEventListener("click",()=>{
+
+//   if(document.querySelector("#form-profile-picture").classList.contains("display")){
+
+//     document.querySelector("#form-profile-picture").classList.remove("display")
+
+//   }else {
+
+//     document.querySelector("#form-profile-picture").classList.add("display")
+
+//   }
+// })
+
+
+
+const burguer = document.getElementById("burguer")
+console.log(burguer)
+burguer.addEventListener('click', function() {
+  this.classList.toggle('open');
+  document.querySelector("header nav").classList.toggle('open')
+
 });
 
-document.querySelector("#show-form").addEventListener("click",()=>{
-
-  if(document.querySelector("#form-profile-picture").classList.contains("display")){
-
-    document.querySelector("#form-profile-picture").classList.remove("display")
-
-  }else {
-
-    document.querySelector("#form-profile-picture").classList.add("display")
-
-  }
-
-
-
-})
